c2032c1fb5a97bb2f35c235b74c7faf4e83403ed
nawm77 <alpha050603@gmail.com>
2023-07-13 18:04:43 +0300

    Add login service

--- src/main/java/com/example/rest_practice/Service/LoginService.java
+++ src/main/java/com/example/rest_practice/Service/LoginService.java
@@ -1,7 +1,6 @@
 package com.example.rest_practice.Service;
 
 import com.example.rest_practice.DTO.Request.LoginRequest;
-import com.example.rest_practice.Exception.UserNotFoundException;
 
 public interface LoginService {
     String loginProcessor(LoginRequest loginRequest) throws Exception;
