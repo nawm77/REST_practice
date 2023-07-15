8ec567cf5de816a4e9b20e40a0cdcc496c2535ac
nawm77 <alpha050603@gmail.com>
2023-07-15 20:16:29 +0300

    Change principal arguments to userDetails

--- src/main/java/com/example/rest_practice/Service/BikeService.java
+++ src/main/java/com/example/rest_practice/Service/BikeService.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.AccessDeniedException;
-import java.security.Principal;
 import java.util.List;
 
 @Service
