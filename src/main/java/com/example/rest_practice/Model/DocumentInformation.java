4250f6d6e1c34912b6bf8f1603d04a43bd5556bc
nawm77 <alpha050603@gmail.com>
2023-07-18 02:50:16 +0300

    Add validation for DTO and Entity

--- src/main/java/com/example/rest_practice/Model/DocumentInformation.java
+++ src/main/java/com/example/rest_practice/Model/DocumentInformation.java
@@ -3,7 +3,6 @@ package com.example.rest_practice.Model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
