a6d6460f26348b3f45d427f8b9e7545eea018d0b
nawm77 <alpha050603@gmail.com>
2023-07-18 20:02:34 +0300

    BugFix

--- src/main/java/com/example/rest_practice/DTO/Request/LoginRequest.java
+++ src/main/java/com/example/rest_practice/DTO/Request/LoginRequest.java
@@ -2,13 +2,13 @@ package com.example.rest_practice.DTO.Request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "Email is required")
     @Size(min = 4, max = 32, message = "Email length must be more than 4")
